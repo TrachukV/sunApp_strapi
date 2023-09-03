88cb9f0123214caa022de9cb8abe5b3e3d7e2b17
nytik <crashgamesua@gmail.com>
2023-09-03 18:00:45 +0300

    Added remote server

--- config/plugins.js
+++ config/plugins.js
@@ -1,33 +1,36 @@
 module.exports = {
-    documentation: {
-      enabled: true,
-      config: {
-        openapi: '3.0.0',
-        info: {
-          version: '1.0.0',
-          title: 'DOCUMENTATION',
-          description: '',
-          termsOfService: 'YOUR_TERMS_OF_SERVICE_URL',
-          contact: {
-            name: 'TEAM',
-            email: 'contact-email@something.io',
-            url: 'sunapp'
-          },
-          license: {
-            name: 'Apache 2.0',
-            url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
-          },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.0',
+        title: 'DOCUMENTATION',
+        description: '',
+        termsOfService: 'YOUR_TERMS_OF_SERVICE_URL',
+        contact: {
+          name: 'TEAM',
+          email: 'contact-email@something.io',
+          url: 'sunapp'
         },
-        'x-strapi-config': {
-          plugins: [ 'upload', 'users-permissions'],
-          path: '/documentation',
+        license: {
+          name: 'Apache 2.0',
+          url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
         },
-        servers: [{ url: 'http://localhost:1337/api', description: 'Development server' }],
-        externalDocs: {
-          description: 'Find out more',
-          url: 'https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html'
-        },
-        security: [ { bearerAuth: [] } ]
-      }
+      },
+      'x-strapi-config': {
+        plugins: ['upload', 'users-permissions'],
+        path: '/documentation',
+      },
+      servers: [
+        { url: 'http://localhost:1337/api', description: 'Development server' },
+        { url: 'https://strapi-7p11.onrender.com/api', description: 'Remote server' },
+      ],
+      externalDocs: {
+        description: 'Find out more',
+        url: 'https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html'
+      },
+      security: [{ bearerAuth: [] }]
     }
   }
+}
